ac1df3bd7b7201e8207dca53be8426b6e284d909
Acer . <alinazwa38@gmail.com>
2026-09-20 15:21:31 +0800

    Initial commit

---
+++ LuasSegitiga.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author Al
+ */
+public class LuasSegitiga {
+
+    public static void main(String[] args) {
+        float alas = 5;
+        int tinggi = 3;
+        float luas = (alas * tinggi) / 2;
+        System.out.println("Luas segitiga = " + luas);
+
+    }
+}
